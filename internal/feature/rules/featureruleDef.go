fb2c0f29ed98490dafdcfdf63a875a043c24737d
Joan Miquel Espada <joanmi@espada.cat>
2021-08-13 00:32:40 +0200

    removed experimentation and ACL

--- internal/feature/rules/featureruleDef.go
+++ internal/feature/rules/featureruleDef.go
@@ -6,22 +6,24 @@ type KeyValue struct {
 }
 
 type FeatureRule struct {
-	Environment     []string `yaml:"environment"`
-	QueryString     KeyValue `yaml:"querystring"`
-	Header          KeyValue `yaml:"header"`
-	Platform        []string `yaml:"platform"`
-	Version         []string `yaml:"version"`
-	Country         []string `yaml:"country"`
-	City            []string `yaml:"city"`
-	UserId          []string `yaml:"userId"`
-	UserGroup       []string `yaml:"userGroup"`
-	Experimentation struct {
-		Id    string `yaml:"id"`
-		Range struct {
-			lowestScore  string `yaml:"lowestScore"`
-			highestScore string `yaml:"highestScore"`
-		} `yaml:"range"`
-		Score []string `yaml:"score"`
-	} `yaml:"experimentation"`
-	FeatureTm FeatureTimer `yaml:"timer"`
+	Environment []string     `yaml:"environment"`
+	QueryString KeyValue     `yaml:"querystring"`
+	Header      KeyValue     `yaml:"header"`
+	Platform    []string     `yaml:"platform"`
+	Version     []string     `yaml:"version"`
+	Country     []string     `yaml:"country"`
+	City        []string     `yaml:"city"`
+	UserId      []string     `yaml:"userId"`
+	UserGroup   []string     `yaml:"userGroup"`
+	FeatureTm   FeatureTimer `yaml:"timer"`
+	/*
+		Experimentation struct {
+			Id    string `yaml:"id"`
+			Range struct {
+				lowestScore  string `yaml:"lowestScore"`
+				highestScore string `yaml:"highestScore"`
+			} `yaml:"range"`
+			Score []string `yaml:"score"`
+		} `yaml:"experimentation"`*/
+
 }
